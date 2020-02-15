8645e1713ba4dee551390259403b922e59c65a8d
James Athanasiou <james.athanasiou@live.com>
2020-02-15 08:54:53 -0500

    Remove mongoose warnings

--- models/index.js
+++ models/index.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
 mongoose.set("debug", true);
 mongoose.Promise = Promise;
+// To fix deprication warnings
+mongoose.set("useNewUrlParser", true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
 mongoose.connect( process.env.DATABASEURL || "mongodb://localhost/me2big");
 
 module.exports.User = require("./user");
